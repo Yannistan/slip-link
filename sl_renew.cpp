8c8b3f2f74abb84e94e8b3581efd5dd4e2f3ac8e
Yannistan <65901883+Yannistan@users.noreply.github.com>
2021-09-08 22:28:11 +0300

    Update sl_renew.cpp

--- sl_renew.cpp
+++ sl_renew.cpp
@@ -20,30 +20,29 @@ int nc_new,nnc_new;
 int bead_index, sup_index, id_sup;
 FILE *renew;
 sigma3=(sqrt(Ns)*b)/sqrt(3.0);
-//renew=fopen("renew.dat","a+");
-//fprintf(renew,"istep=%d\n",istep);
+
 	
         for (j=0;j<zzz;j++) {
         /* Destruction-recreation of the paired sliplink: binary correspondence between sliplinks j& jj */
-	jj=j+1;
-        if (j%2==1) jj=j-1;
-        if (destroy[j]==1) {
-//	fprintf(renew,"SL %d destroyed!\n",j);
+		jj=j+1;
+        	if (j%2==1) jj=j-1;
+        		if (destroy[j]==1) {
+
         /* Choose a new monomer among the n_ch chains */
-	frac = drand48();
+		frac = drand48();
 	do {
-        id=rand()%(n_ch*n_m);
+        	id=rand()%(n_ch*n_m);
 	} while ( bead[g_bead_index[id]].terminal==-1) ;
 	sl[j].id_left_bead=id;
         /* Slip-link j recreated  */
-	bead_index=g_bead_index[id];
-	id_sup = bead[bead_index].id_sup;
-	sup_index = g_bead_index[id_sup];
-	sl[j].truncxj=bead[bead_index].index_in_chain;
-        sl[j].xj=(double) (sl[j].truncxj)+frac;
-	frac=sl[j].xj - (double) (sl[j].truncxj);	
+		bead_index=g_bead_index[id];
+		id_sup = bead[bead_index].id_sup;
+		sup_index = g_bead_index[id_sup];
+		sl[j].truncxj=bead[bead_index].index_in_chain;
+        	sl[j].xj=(double) (sl[j].truncxj)+frac;
+		frac=sl[j].xj - (double) (sl[j].truncxj);	
    		for (k=0;k<3;k++) {
-        	sl[j].XYZsj[k]=bead[bead_index].XYZ[k]+(frac)*(bead[sup_index].XYZ[k]-bead[bead_index].XYZ[k]);
+        		sl[j].XYZsj[k]=bead[bead_index].XYZ[k]+(frac)*(bead[sup_index].XYZ[k]-bead[bead_index].XYZ[k]);
 	//	  fprintf(renew,"new sj coordinates for j=%d are:%lf %lf %lf\n",j,sl[j].XYZsj[0],sl[j].XYZsj[1],sl[j].XYZsj[2]);
 		}
           	for (k=0;k<3;k++) {
@@ -63,29 +62,29 @@ sigma3=(sqrt(Ns)*b)/sqrt(3.0);
         frac=drand48();
 	w1=drand48();	
         if (w1-0.5>0.000001) {
-	idjj=(nnc_new)*(n_m);
-	sl[jj].id_left_bead=idjj;
-	bead_index=g_bead_index[idjj];
-	id_sup = bead[bead_index].id_sup;
-	sup_index=g_bead_index[id_sup];
-	sl[jj].truncxj=bead[bead_index].index_in_chain;
-        sl[jj].xj=sl[jj].truncxj+frac;
+		idjj=(nnc_new)*(n_m);
+		sl[jj].id_left_bead=idjj;
+		bead_index=g_bead_index[idjj];
+		id_sup = bead[bead_index].id_sup;
+		sup_index=g_bead_index[id_sup];
+		sl[jj].truncxj=bead[bead_index].index_in_chain;
+        	sl[jj].xj=sl[jj].truncxj+frac;
         } else {
 		if (nnc_new==0)
-		idjj=n_m-2;
+			idjj=n_m-2;
 		else  idjj=(nnc_new)*(n_m)-2; 
-	sl[jj].id_left_bead=idjj;
-	bead_index=g_bead_index[idjj];
-	id_sup = bead[bead_index].id_sup;
-	sup_index=g_bead_index[id_sup];
-	sl[jj].truncxj=bead[bead_index].index_in_chain;
-        sl[jj].xj=sl[jj].truncxj+frac;
+		sl[jj].id_left_bead=idjj;
+			bead_index=g_bead_index[idjj];
+		id_sup = bead[bead_index].id_sup;
+		sup_index=g_bead_index[id_sup];
+		sl[jj].truncxj=bead[bead_index].index_in_chain;
+        	sl[jj].xj=sl[jj].truncxj+frac;
         }
 
    for (k=0;k<3;k++) {
         sl[jj].XYZsj[k]=bead[bead_index].XYZ[k]+(frac)*(bead[sup_index].XYZ[k]-bead[bead_index].XYZ[k]);
-	//	  fprintf(renew,"new sj coordinates for jj=%d are:%lf %lf %lf\n",jj,sl[jj].XYZsj[0],sl[jj].XYZsj[1],sl[jj].XYZsj[2]);
-}
+	
+   }
             for (k=0;k<3;k++) {
                         w1=ran1(in_ran1);
                         w2=ran1(in_ran1);
@@ -98,6 +97,6 @@ sigma3=(sqrt(Ns)*b)/sqrt(3.0);
             }
         } /*endif destroy[j] */
         }
-//fclose(renew);
+
 }
 
